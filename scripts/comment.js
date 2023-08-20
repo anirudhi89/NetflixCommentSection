e6d760216e2ee1070b57c6df54cdbeb6a8d48e6f
anirudhi89 <anirudh.iyer@outlook.com>
2023-08-20 02:51:27 -0700

    ''

--- scripts/comment.js
+++ scripts/comment.js
@@ -1,14 +1,10 @@
 class Comment {
     constructor(id, author, message, createdDate, startTime, endTime) {
-        this.id = id;
         this.author = author;
         this.message = message;
         this.createdDate = createdDate;
         this.startTime = this.startTime;
         this.endTime = this.endTime;
-    }
-    get id() {
-        return this.id;
     }
     get author() {
         return this.author;
